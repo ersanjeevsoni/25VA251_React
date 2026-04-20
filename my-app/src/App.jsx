51a4c908dbda9a3e50028ca04affb9d1c68c2d64
Ersanjeev Soni <ersanjeevsoni@gmail.com>
2026-04-20 10:13:44 +0530

    exp 7 and exp 8

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,16 +1,17 @@
 
-import UseEffect from "./Component/Hooks/UseEffect";
-import UseState from "./Component/Hooks/UseState";
-import UseStateHook from "./Component/Hooks/UseStateHook";
 import Props from "./Component/Props/Props";
+import UseState from "./Component/Hooks/UseState";
+import UseEffect from "./Component/Hooks/UseEffect";
 
-// import Exp7Count from "./Lab/Exp7/Exp7Count";
-// import Form from "./Lab/Exp8/Form";
+
+import StudentCard from "./Lab/Exp7/StudentCard";
+import Count from "./Lab/Exp8/Count";
+
 
 
 
 function App() {
- /*  const students = [
+/*   const students = [
     { name: "Abhay", rollno: "250B1034", course: "B.Tech" },
     { name: "Vikas", rollno: "250B1035", course: "MCA" },
     { name: "Rahul", rollno: "250B1036", course: "BCA" }
@@ -20,12 +21,23 @@ function App() {
     <>
     {/* <Props name="Abhay" rollno="250B1034" course="B.Tech"/> */}
     {/* for multiple data */}
-{/* <Props students={students} /> */}
-    <UseState/>
+    {/* <Props students={students} /> */}
+    {/* <UseState/> */}
     {/* <UseEffect/> */}
-    {/* <Exp7Count HeadingText="React JS"/> */}
-    {/* <Form/> */}
-    {/* <UseStateHook/> */}
+
+    {/* //Experiment 07 */}
+      {/*  <h1>Student Information System</h1>
+      <div className="student-container">
+        <StudentCard name="Monika" course="B.Tech Data Science" marks="95" />
+        <StudentCard name="Rahul" course="B.Tech CS" marks="88" />
+        <StudentCard name="Sneha" course="B.Tech IT" marks="92" />
+      </div> */}
+
+      {/* Experiment 08 */}
+      <Count/>
+
+ 
+
     
    
    
--- my-app/src/Component/Hooks/UseState.jsx
+++ my-app/src/Component/Hooks/UseState.jsx
@@ -1,60 +1,78 @@
-import React, { useState } from "react";
-import "./UseState.css";
+import React, { useState } from 'react'
+import './UseState.css';
 
 function UseState() {
-  const [color, setColor] = useState("blue");
 
-  const changeColor = () => {
-    // setColor('red');
-    setColor(color == "blue" ? "red" : "blue");
-  };
 
-  //counter using use state
-  const [count, setCount] = useState(0);
+ // useState for state in functional component for singel key and change on click event
+ const[branch,setBranch]=useState("B.Tech");
+ const changeBranch=()=>{
+    setBranch("B.Tect(AIML)");
+ }
+ // useState to change color
+ const[color,setColor]=useState('blue');
 
-  const increase = () => {
-    setCount((count) => count + 1);
-  };
-  const decrease = () => {
-    setCount((count) => count - 1);
-  };
-  const reset = () => {
-    setCount(0);
-  };
-  //timer
-  const [time, setTime] = useState(0);
+ const chnageColor=()=>{
+    setColor(color==='blue'?'red':'blue')
+ }
 
-  //   if (time < 10) { // limit to avoid infinite loop
-  setTimeout(() => {
-    setTime(time + 1);
-  }, 1000);
-  // instead of useState for timer use , useEffect
-  /*  useState → for storing data 
-useEffect → for side effects (API, timers, subscriptions) */
+   // useState having multiple keys
+ const[state,setState]=useState({
+    name:"Abhay",
+    age:21
+ })
+ //if we want to change multiple keys on any event then pass function
+ const mouseOver=()=>{
+    setState({
+        ...state, // keep existing values
+        name:"Vinay",  // update name
+        age:23  //update age
+    });
+ }
+// useState to change count
+ const[count,setCount]=useState(0);
+ const increment=()=>{
+    setCount(count+1)
+ };
+ const decrement=()=>{
+    setCount(count-1)
+ };
+ const reset=()=>{
+    setCount(count*0)
+ };
+
+ //timer
+   const [time, setTime] = useState(0);
+ 
+   //   if (time < 10) { // limit to avoid infinite loop
+   setTimeout(() => {
+     setTime(time + 1);
+   }, 1000);
+   // instead of useState for timer use , useEffect
+   /*  useState → for storing data 
+ useEffect → for side effects (API, timers, subscriptions) */
 
   return (
-    <>
-      <h2>Hooks</h2>
-      <p>
-        React Hooks are special JavaScript functions introduced in version 16.8
-        that allow you to use state and other features (like lifecycle methods
-        and context) inside functional components without writing a class.
-      </p>
-      <h3>UseState</h3>
-      <p style={{ color: color }} onMouseOver={changeColor}>
-        Adds local state to functional components. It returns a pair: the
-        current state value and a function to update it. The first value, color,
-        is our current state. The second value, setColor, is the function that
-        is used to update our state.
-      </p>
-      <h2>Counter {count}</h2>
-      <button onClick={increase}>Increase</button>
-      <button onClick={decrease}>Decrease</button>
-      <button onClick={reset}>Reset</button>
+    <div class="container">
+  
 
-      <h2>Timer {time}</h2>
-    </>
-  );
+    {/* state change name and change color*/}
+    <h2 onMouseOver={mouseOver} onMouseOut={chnageColor} style={{color:color}}>
+    Hello, My name is {state.name} and age is {state.age}</h2>
+
+    {/* only change branch on click event */}
+    <h3>My course is {branch}</h3>
+    <button onClick={changeBranch} className='buttons'>Change Branch</button>
+
+    <div>
+    <h2>Change Count {count}</h2>
+    <button className='buttons' onClick={increment}>+VE</button>
+        <button className='buttons' onClick={decrement}>-VE</button>
+            <button className='buttons' onClick={reset}>Reset</button>
+            </div>
+    <h2>Timer {time}</h2>
+    </div>
+  )
 }
 
-export default UseState;
+export default UseState
